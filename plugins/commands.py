f7d270c6ed46d6593f5d4b91d21a8c3da2960ed9
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2021-10-20 07:04:59 +0530

    Add files via upload

---
+++ plugins/commands.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+# Copyright (C) @S780821
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import Client, filters
+from utils import USERNAME, mp
+from config import Config
+U=USERNAME
+CHAT=Config.CHAT
+msg=Config.msg
+HOME_TEXT = "<b>Hello, [{}](tg://user?id={})\n\nI am HANGOVER_RADIO 24×7.\n\nHits /help for more details...</b>"
+HELP = """
+**User Commands:**
+▷/play **[song name]/[yt link]**: Reply to an audio file.
+▷/dplay **[song name]:** Play music from Deezer.
+▷/player:  Show current playing song.
+▷upload: Uploads current playing song as audio file.
+▷/help: Show help for commands.
+▷/playlist: Shows the playlist.
+
+**Admin Commands:**
+▷/skip **[n]** ...  Skip current or n where n >= 2
+▷cplay: Play music from a channel's music files.
+▷/yplay: Play music from a youtube playlist.
+▷/join: Join voice chat.
+▷/leave: Leave current voice chat.
+▷/shuffle: Shuffle Playlist.
+▷/vc: Check which VC is joined.
+▷/stop: Stop playing.
+▷/radio: Start Radio.
+▷/stopradio: Stops Radio Stream.
+▷/clearplaylist: Clear the playlist.
+▷/export: Export current playlist for future use.
+▷/import: Import a previously exported playlist.
+▷/replay: Play from the beginning.
+▷/clean: Remove unused RAW PCM files.
+▷/pause: Pause playing.
+▷/resume: Resume playing.
+▷/volume: Change volume(0-200).
+▷/mute: Mute in VC.
+▷/unmute: Unmute in VC.
+▷/restart: Restarts the Bot.
+"""
+
+
+
+@Client.on_message(filters.command(['start', f'start@{U}']))
+async def start(client, message):
+    buttons = [
+        [
+        InlineKeyboardButton("🔥 Source Code 🔥", url='https://github.com/S780821/HANGOVER_RADIO'),
+    ],
+    [
+        InlineKeyboardButton('👥 Group', url='https://t.me/xmarty_support'),
+        InlineKeyboardButton('CHIT CHAT', url='https://t.me/welcomefriendclub'),
+    ],
+    [
+        InlineKeyboardButton('🆘 Help & Commands 🆘', callback_data='help'),
+
+    ]
+    ]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await message.reply(HOME_TEXT.format(message.from_user.first_name, message.from_user.id), reply_markup=reply_markup)
+    await message.delete()
+
+
+
+@Client.on_message(filters.command(["help", f"help@{U}"]))
+async def show_help(client, message):
+    buttons = [
+        [
+            InlineKeyboardButton("🔥 Source Code 🔥", url='https://github.com/S780821/HANGOVER_RADIO'),
+        ],
+        [
+            InlineKeyboardButton('👥 Group', url='https://t.me/xmarty_support'),
+            InlineKeyboardButton('CHIT CHAT ', url='https://t.me/welcomefriendclub'),
+        ],
+        [
+            InlineKeyboardButton(' OWNER 🔰', url='https://t.me/Xmartperson'),
+        
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await message.reply_text(
+        HELP,
+        reply_markup=reply_markup
+        )
+    await message.delete()
+@Client.on_message(filters.command(["restart", f"restart@{U}"]) & filters.user(Config.ADMINS))
+async def restart(client, message):
+    await message.reply_text("🔄 Restarting...")
+    await message.delete()
+    process = FFMPEG_PROCESSES.get(CHAT)
+    if process:
+        try:
+            process.send_signal(SIGINT)
+        except subprocess.TimeoutExpired:
+            process.kill()
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
---
+++ plugins/inline.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# Copyright (C) @S780821
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+from pyrogram.handlers import InlineQueryHandler
+from youtubesearchpython import VideosSearch
+from utils import USERNAME
+from pyrogram.types import InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import Client, errors
+from config import Config
+REPLY_MESSAGE=Config.REPLY_MESSAGE
+buttons = [
+    [
+        InlineKeyboardButton('🏃 Deploy to Heroku 🏃', url='https://heroku.com/deploy?template=https://github.com/S780821/HANGOVER_RADIO'),
+    ],
+    [
+        InlineKeyboardButton('👨‍🎤 Play Music', url=f'https://t.me/{USERNAME}'),
+        InlineKeyboardButton('Owner🤓', url='https://t.me/xmartperson'),
+    ],
+    [
+        InlineKeyboardButton('🆘 Help & Commands 🆘', callback_data='help')       
+    ]
+    ]
+@Client.on_inline_query()
+async def search(client, query):
+    answers = []
+    if query.query == "ORU_MANDAN_PM_VANNU":
+        answers.append(
+            InlineQueryResultArticle(
+                title="Deploy",
+                input_message_content=InputTextMessageContent(f"{REPLY_MESSAGE}\n\n<b>You can't use this bot in your group, for that you have to make your own bot from the [Source Code](https://github.com/S780821/HANGOVER_RADIO) below.</b>", disable_web_page_preview=True),
+                reply_markup=InlineKeyboardMarkup(buttons)
+                )
+            )
+        await query.answer(results=answers, cache_time=0)
+        return
+    string = query.query.lower().strip().rstrip()
+    if string == "":
+        await client.answer_inline_query(
+            query.id,
+            results=answers,
+            switch_pm_text=("Search a YouTube Videos"),
+            switch_pm_parameter="help",
+            cache_time=0
+        )
+    else:
+        videosSearch = VideosSearch(string.lower(), limit=50)
+        for v in videosSearch.result()["result"]:
+            answers.append(
+                InlineQueryResultArticle(
+                    title=v["title"],
+                    description=("Duration: {} Views: {}").format(
+                        v["duration"],
+                        v["viewCount"]["short"]
+                    ),
+                    input_message_content=InputTextMessageContent(
+                        "/play https://www.youtube.com/watch?v={}".format(
+                            v["id"]
+                        )
+                    ),
+                    thumb_url=v["thumbnails"][0]["url"]
+                )
+            )
+        try:
+            await query.answer(
+                results=answers,
+                cache_time=0
+            )
+        except errors.QueryIdInvalid:
+            await query.answer(
+                results=answers,
+                cache_time=0,
+                switch_pm_text=("Nothing found"),
+                switch_pm_parameter="",
+            )
+
+
+__handlers__ = [
+    [
+        InlineQueryHandler(
+            search
+        )
+    ]
+]
