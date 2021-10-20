d21f1c17737e722c9f5740f27cbb7d8d50b963ea
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2021-10-20 10:00:47 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-echo "Cloning Repo...."
-git clone https://github.com/780821/HANGOVER_RADIO.git /HANGOVER_RADIO
-cd /HANGOVER_RADIO
-pip3 install -U -r requirements.txt
-echo "Starting Bot...."
-python3 main.py
